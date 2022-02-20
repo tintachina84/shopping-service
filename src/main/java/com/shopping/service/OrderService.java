0a934025c09b76a5b7e9166035c1a89369b2ca0f
SeongHyeon.Seo <tintachina84@gmail.com>
2022-02-20 21:52:53 +0900

    Added order history page.

--- src/main/java/com/shopping/service/OrderService.java
+++ src/main/java/com/shopping/service/OrderService.java
@@ -1,14 +1,17 @@
 package com.shopping.service;
 
 import com.shopping.dto.OrderDto;
-import com.shopping.entity.Item;
-import com.shopping.entity.Member;
-import com.shopping.entity.Order;
-import com.shopping.entity.OrderItem;
+import com.shopping.dto.OrderHistDto;
+import com.shopping.dto.OrderItemDto;
+import com.shopping.entity.*;
+import com.shopping.repository.ItemImgRepository;
 import com.shopping.repository.ItemRepository;
 import com.shopping.repository.MemberRepository;
 import com.shopping.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +27,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto, String email) {
         Item item = this.itemRepository.findById(orderDto.getItemId())
@@ -37,5 +41,26 @@ public class OrderService {
         this.orderRepository.save(order);
 
         return order.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+        List<Order> orders = this.orderRepository.findOrders(email, pageable);
+        Long totalCount = this.orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+        for (Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+            for (OrderItem orderItem : orderItems) {
+                ItemImg itemImg = this.itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
     }
 }
